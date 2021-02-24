89bd24a4cbc8eaff5ab3f8e98704b468298506b9
Kauan Lazzarin <kauanlazzarin@gmail.com>
2021-02-24 20:49:07 -0300

    migração para nextJS

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import ExperienceBar from "../components/ExperienceBar";
+export default function Index (){
+    return (
+        <div className="container">
+            <ExperienceBar />
+        </div>
+    )
+};
